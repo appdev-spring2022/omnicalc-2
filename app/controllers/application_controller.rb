deb89dda25b4055101d9c6569465d75487252e3d
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-04-28 15:11:43 +0000

    V1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,37 @@
 class ApplicationController < ActionController::Base
+  def add
 
+    @first = params.fetch("alice").to_f
+    @second = params.fetch("carol").to_f
+    @result = @first + @second
+
+    render(:template => "some_templates/add.html.erb")
+  end
+
+  def subtract
+
+    @first = params.fetch("alice").to_f
+    @second = params.fetch("carol").to_f
+    @result = @first - @second
+
+    render(:template => "some_templates/subtract.html.erb")
+  end
+
+  def multiply
+
+    @first = params.fetch("alice").to_f
+    @second = params.fetch("carol").to_f
+    @result = @first * @second
+
+    render(:template => "some_templates/multiply.html.erb")
+  end
+
+  def divide
+
+    @first = params.fetch("alice").to_f
+    @second = params.fetch("carol").to_f
+    @result = @first/@second
+
+    render(:template => "some_templates/divide.html.erb")
+  end
 end
